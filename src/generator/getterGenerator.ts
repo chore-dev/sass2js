88df1badd60698c004d58f8cc9c5093e2e4107c4
Issac Li <issac.li.523@gmail.com>
2024-06-14 18:17:07 +0800

    fix: fixed relative path missing prefix issue

--- src/generator/getterGenerator.ts
+++ src/generator/getterGenerator.ts
@@ -10,13 +10,14 @@ function getterGenerator(
   const { inputPath, outputPath } = paths;
 
   const outputDir = outputPath.split('/').slice(0, -1).join('/');
+  const variablesPath = relative(outputDir, inputPath);
 
   return implementGetters
     ? [
         // TODO: Fill in scss path
         `import parser from '@chore-dev/sass2ts/parser';`,
         '',
-        `import variables from '${relative(outputDir, inputPath)}';`,
+        `import variables from '${/^\/|\.\//.test(variablesPath) ? './' : ''}${variablesPath}';`,
         '',
         `export const get${rootName} = () => {`,
         `  return parser<${rootName}>(variables);`,
